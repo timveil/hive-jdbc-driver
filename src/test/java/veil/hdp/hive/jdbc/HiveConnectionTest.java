ba07971b261fd342d6d02b5ae33983bc041e2764
timveil <tjveil@gmail.com>
2017-03-09 22:04:46 -0500

    more cleanup

---
+++ src/test/java/veil/hdp/hive/jdbc/HiveConnectionTest.java
@@ -0,0 +1,35 @@
+package veil.hdp.hive.jdbc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HiveConnectionTest extends BaseJunitTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void close() throws Exception {
+
+    }
+
+    @Test
+    public void isClosed() throws Exception {
+
+    }
+
+    @Test
+    public void createStatement() throws Exception {
+
+    }
+
+
+}
