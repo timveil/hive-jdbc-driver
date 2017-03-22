0045cec0b3fba20b453104fb804422eb30e99878
Tim Veil <tjveil@gmail.com>
2017-03-21 21:52:23 -0400

    more datasource

--- src/main/java/veil/hdp/hive/jdbc/HiveDataSource.java
+++ src/main/java/veil/hdp/hive/jdbc/HiveDataSource.java
@@ -10,13 +10,11 @@ public class HiveDataSource extends AbstractDataSource {
 
     private static final Logger log = LoggerFactory.getLogger(HiveDataSource.class);
 
-    private String databaseName;
-    private String dataSourceName;
-    private String description;
-    private String networkProtocol;
-    private String password;
     private String serverName;
+    private String databaseName;
     private String user;
+    private String password;
+    private int portNumber;
 
     @Override
     public Connection getConnection() throws SQLException {
@@ -28,59 +26,5 @@ public class HiveDataSource extends AbstractDataSource {
         return super.getConnection(username, password);
     }
 
-    public String getDatabaseName() {
-        return databaseName;
-    }
 
-    public void setDatabaseName(String databaseName) {
-        this.databaseName = databaseName;
-    }
-
-    public String getDataSourceName() {
-        return dataSourceName;
-    }
-
-    public void setDataSourceName(String dataSourceName) {
-        this.dataSourceName = dataSourceName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getNetworkProtocol() {
-        return networkProtocol;
-    }
-
-    public void setNetworkProtocol(String networkProtocol) {
-        this.networkProtocol = networkProtocol;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getServerName() {
-        return serverName;
-    }
-
-    public void setServerName(String serverName) {
-        this.serverName = serverName;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
 }
