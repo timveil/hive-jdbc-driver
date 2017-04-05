d1c404da73e63bd5483f2d5f4fd53cd8b4702b0c
Tim Veil <tjveil@gmail.com>
2017-04-04 22:38:25 -0400

    cleanup

--- src/main/java/veil/hdp/hive/jdbc/data/VarcharColumn.java
+++ src/main/java/veil/hdp/hive/jdbc/data/VarcharColumn.java
@@ -2,24 +2,9 @@ package veil.hdp.hive.jdbc.data;
 
 import veil.hdp.hive.jdbc.metadata.ColumnDescriptor;
 
-import java.sql.SQLException;
+public class VarcharColumn extends StringColumn {
 
-public class VarcharColumn extends BaseColumn<String> {
     VarcharColumn(ColumnDescriptor descriptor, String value) {
         super(descriptor, value);
-    }
-
-    @Override
-    public String asString() throws SQLException {
-        return getValue();
-    }
-
-    @Override
-    public byte[] asByteArray() throws SQLException {
-        if (value != null) {
-            return value.getBytes();
-        }
-
-        return null;
     }
 }
