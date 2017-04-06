947dd4b3354cafd80a2520896edda7275de054e0
timveil <tjveil@gmail.com>
2017-04-05 20:49:00 -0400

    moving back to thrift 0.9.3

--- binary-driver/src/test/java/veil/hdp/hive/jdbc/BinaryDriverTest.java
+++ binary-driver/src/test/java/veil/hdp/hive/jdbc/BinaryDriverTest.java
@@ -1,68 +1,14 @@
 package veil.hdp.hive.jdbc;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import java.sql.Driver;
+import java.sql.SQLException;
 
-import java.sql.Connection;
-import java.util.Properties;
+public class BinaryDriverTest extends BaseDriverTest {
 
-import static java.lang.Class.forName;
-
-public class BinaryDriverTest {
-
-
-    private BinaryHiveDriver hiveDriver = null;
-
-    @Before
-    public void setUp() throws Exception {
-        hiveDriver = new BinaryHiveDriver();
+    @Override
+    Driver createDriver() throws SQLException {
+        return new BinaryHiveDriver();
     }
 
-    @After
-    public void tearDown() throws Exception {
-        hiveDriver = null;
-    }
-
-    @Test
-    public void connect() throws Exception {
-
-        Connection connection = null;
-        String url = null;
-
-        Properties properties = new Properties();
-        properties.setProperty("user", "hive");
-
-        url = "jdbc:hive2://hive.hdp.local:10000/default";
-
-        try {
-            connection = hiveDriver.connect(url, properties);
-
-            Assert.assertNotNull(connection);
-        } finally {
-
-            if (connection != null) {
-                connection.close();
-            }
-        }
-
-
-    }
-
-    @Test
-    public void acceptsURL() throws Exception {
-        String url = "jdbc:hive2://foo";
-
-        boolean acceptsURL = hiveDriver.acceptsURL(url);
-
-        Assert.assertTrue(acceptsURL);
-
-        url = "jdbc:mysql://foo";
-
-        acceptsURL = hiveDriver.acceptsURL(url);
-
-        Assert.assertFalse(acceptsURL);
-    }
 
 }
---
+++ test-utils/src/main/java/veil/hdp/hive/jdbc/BaseDriverTest.java
@@ -0,0 +1,70 @@
+package veil.hdp.hive.jdbc;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public abstract class BaseDriverTest  extends BaseUnitTest {
+
+    protected Driver driver;
+
+    abstract Driver createDriver() throws SQLException;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = createDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver = null;
+        }
+    }
+
+    @Test
+    public void connect() throws Exception {
+
+        Connection connection = null;
+        String url = null;
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "hive");
+
+        url = "jdbc:hive2://hive.hdp.local:10000/default";
+
+        try {
+            connection = driver.connect(url, properties);
+
+            Assert.assertNotNull(connection);
+        } finally {
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+
+    }
+
+    @Test
+    public void acceptsURL() throws Exception {
+        String url = "jdbc:hive2://foo";
+
+        boolean acceptsURL = driver.acceptsURL(url);
+
+        Assert.assertTrue(acceptsURL);
+
+        url = "jdbc:mysql://foo";
+
+        acceptsURL = driver.acceptsURL(url);
+
+        Assert.assertFalse(acceptsURL);
+    }
+}
