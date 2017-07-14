f87dc06cede6f046974ba348f21bf26314eced30
timveil <tjveil@gmail.com>
2017-07-13 23:06:30 -0400

    adding JUL bridge support so we can pick up some additional kerberos debugging during testing.  this would not be suitable for production use because of performance implications

--- test-tools/src/main/java/veil/hdp/hive/jdbc/test/BaseUnitTest.java
+++ test-tools/src/main/java/veil/hdp/hive/jdbc/test/BaseUnitTest.java
@@ -2,11 +2,22 @@ package veil.hdp.hive.jdbc.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+
 
 public class BaseUnitTest {
 
     protected static final Logger log = LoggerFactory.getLogger(BaseUnitTest.class);
 
+    public BaseUnitTest() {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
+        java.util.logging.Logger.getLogger("javax.security.sasl").setLevel(java.util.logging.Level.FINEST);
+
+
+    }
 
     public static String getHost() {
         return System.getProperty("test.host");
