d39e0641bff546ecac2bd835aa6de19d1ad03010
timveil <tjveil@gmail.com>
2017-07-15 19:57:35 -0400

    added missing functional interface annotation

--- core/src/main/java/veil/hdp/hive/jdbc/core/UriProperties.java
+++ core/src/main/java/veil/hdp/hive/jdbc/core/UriProperties.java
@@ -3,6 +3,7 @@ package veil.hdp.hive.jdbc.core;
 import java.net.URI;
 import java.util.Properties;
 
+@FunctionalInterface
 public interface UriProperties {
     void load(URI uri, Properties properties);
 }
