392734b04f3d02a2470912cfa3a89441dfc7b235
Tim Veil <tjveil@gmail.com>
2018-07-16 23:06:07 -0400

    adding additional option

--- machines/binary/install.sh
+++ machines/binary/install.sh
@@ -43,15 +43,16 @@ echo " "
 
 sed -i "s/^hostname=localhost/hostname=jdbc-binary.hdp.local/g" /etc/ambari-agent/conf/ambari-agent.ini
 
-# echo " "
-# echo "---------------------------------------------------------------------------------------------------------------"
-# echo "----- install overops"
-# echo "---------------------------------------------------------------------------------------------------------------"
-# echo " "
-
-# curl -sL /dev/null http://get.takipi.com | sudo bash /dev/stdin -i --sk=S36579#bLFSWU3y+KL7s5kP#Ulu1V6TctQOeemsAKFeWiK09lzHRHEkX5HGbx7pL3N4=#386e
-
-# export JAVA_TOOL_OPTIONS="-agentlib:TakipiAgent"
+#echo " "
+#echo "---------------------------------------------------------------------------------------------------------------"
+#echo "----- install overops"
+#echo "---------------------------------------------------------------------------------------------------------------"
+#echo " "
+#
+#curl -sL /dev/null http://get.takipi.com | sudo bash /dev/stdin -i --sk=S36579#bLFSWU3y+KL7s5kP#Ulu1V6TctQOeemsAKFeWiK09lzHRHEkX5HGbx7pL3N4=#386e
+#
+#export JAVA_TOOL_OPTIONS="-agentlib:TakipiAgent"
+#export TAKIPI_ARGS="takipi.no.cerebro"
 
 echo " "
 echo "---------------------------------------------------------------------------------------------------------------"
